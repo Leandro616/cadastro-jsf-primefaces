e41df759f287fea6de955ef9d600e43f921bbb7e
Leandro de Souza Rocha <60828701+Leandro616@users.noreply.github.com>
2023-05-24 12:28:55 -0300

    criando persistence.xml e testando

---
+++ src/test/java/io/github/leandro616/repository/PersistenceTest.java
@@ -0,0 +1,21 @@
+package io.github.leandro616.repository;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class PersistenceTest {
+
+    @Test
+    void conexaoTest() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("empresas");
+        assertNotNull(emf);
+
+        EntityManager em = emf.createEntityManager();
+        assertNotNull(em);
+    }
+}
